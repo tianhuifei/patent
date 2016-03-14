ff461f2f2fae98c89f46ab622cfc14748265642d
AfterWin <CJ_Zheng1023@hotmail.com>
2016-03-14 23:44:55 +0800

    switchButton组件初版

--- support/switchButton/js/jquery.switchButton.js
+++ support/switchButton/js/jquery.switchButton.js
@@ -7,43 +7,49 @@
             whenOff:function(btnObj){},
             defaultState:"off"
         }
-        var me=$(this);
-        var op={
-            onName:me.attr("data-onName")||defaults.onName,
-            offName:me.attr("data-offName")||defaults.offName,
-            whenOn:me.attr("data-whenOn")||defaults.whenOn,
-            whenOff:me.attr("data-whenOff")||defaults.whenOff,
-            defaultState:me.attr("data-defaultState")||defaults.defaultState
-        }
-        var $onName=$("<span>"+op.onName+"</span>").addClass("span-on-name"),
-            $offName=$("<span>"+op.offName+"</span>").addClass("span-off-name"),
-            $button=$("<i></i>").addClass("switch-button").css("left",2);
-        me.addClass("switch-button-bar");
-        if(op.defaultState=="on"){
-            me.addClass("active");
-            $button.css("left",32);
-        }
-
-        me.append($onName).append($button).append($offName);
-        me.bind({
-            click:function(){
-                if(me.hasClass("active")){
-                    $button.animate({
-                        left:2
-                    },function(){
-                        me.removeClass("active");
-                        op.whenOff(me);
-                    })
-                }else{
-                    $button.animate({
-                        left:32
-                    },function(){
-                        me.addClass("active");
-                        op.whenOn(me);
-                    })
-                }
+        $(this).each(function(){
+            var me=$(this);
+            var op={
+                onName:me.attr("data-onName")||defaults.onName,
+                offName:me.attr("data-offName")||defaults.offName,
+                whenOn:me.attr("data-whenOn")||defaults.whenOn,
+                whenOff:me.attr("data-whenOff")||defaults.whenOff,
+                defaultState:me.attr("data-defaultState")||defaults.defaultState
             }
+            var $onName=$("<span>"+op.onName+"</span>").addClass("span-on-name"),
+                $offName=$("<span>"+op.offName+"</span>").addClass("span-off-name"),
+                $button=$("<i></i>").addClass("switch-button").css("left",2);
+            me.addClass("switch-button-bar");
+            if(op.defaultState=="on"){
+                me.addClass("active");
+                $button.css("left",32);
+            }
+
+            me.append($onName).append($button).append($offName);
+            me.bind({
+                click:function(){
+                    if(me.hasClass("active")){
+                        $button.animate({
+                            left:2
+                        },function(){
+                            me.removeClass("active");
+                            op.whenOff(me);
+                        })
+                    }else{
+                        $button.animate({
+                            left:32
+                        },function(){
+                            me.addClass("active");
+                            eval(op.whenOn+"(me)")
+                            //op.whenOn(me);
+                        })
+                    }
+                }
+            })
+
+
         })
+
 
 
     }
--- support/switchButton/js/test.js
+++ support/switchButton/js/test.js
@@ -2,5 +2,5 @@
  * Created by CJ_Zheng on 2016/3/14.
  */
 function whenOn(obj){
-    alert(obj.html())
+
 }
