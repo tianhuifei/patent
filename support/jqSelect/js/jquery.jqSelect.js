21220a3aaea8054af2039c323e46bec8ec05ed0e
AfterWin <CJ_Zheng1023@hotmail.com>
2016-03-17 18:27:19 +0800

    jqSelect控件V1.0.0

---
+++ support/jqSelect/js/jquery.jqSelect.js
@@ -0,0 +1,122 @@
+/**
+ * 使用方法：
+ *
+ *        在页面<div data-width="100" data-height="30" id="test">
+ *                  <div value="123">123</div>
+ *                  <div value="456">456</div>
+ *                  <div value="789" checked="checked">789</div>
+ *              </div>
+ *         data-width为下拉框宽度（必填），data-height为下拉框高度，checked为默认选中
+ *
+ *         在js里$(选择器).jqSelect();初始化控件
+ *
+ *
+ *         @author zhengchj
+ *         @mail zhengchj@neusoft.com
+ *
+ *         update log     v1.0.0     2016.3.17      完成基本功能
+ *
+ *
+ *
+ *
+ */
+
+
+
+(function($){
+
+
+    /**
+     * 扩展Jquery原型链
+     */
+    $.fn.jqSelect=function(){
+        $(this).each(function(){
+            var target=$(this);
+            target.addClass("jq-select-container");
+            var selectItem=target.find("[checked='checked']").length!=0?target.find("[checked='checked']"):target.children().eq(0);
+            var menuField=$("<span>"+selectItem.html()+"</span>").css({
+                "line-height":target.attr("data-height")+"px"
+            });
+            target.attr("value",selectItem.attr("value"));
+            var menu=$("<a></a>").append(menuField).addClass("jq-select-menu").append("<i></i>");
+            var list=$("<ul></ul>");
+            /**
+             * 基于原始dom树构建下拉菜单项
+             * @author zhengchj
+             * @mail zhengchj@neusoft.com
+             */
+            target.children().each(function(){
+                var me=$(this);
+                var item=$("<li></li>");
+                var itemContent=$("<a>"+me.html()+"</a>").attr("value",me.attr("value"));
+                itemContent.click(function(){
+                    itemContent.parent().siblings().find("a").removeClass("active");
+                    itemContent.addClass("active");
+                    menuField.html(itemContent.html());
+                    target.attr("value",itemContent.attr("value"));
+                })
+                item.append(itemContent);
+                list.append(item);
+                list.find("li").eq(selectItem.index()).find("a").addClass("active");
+
+            })
+            target.width(Number(target.attr("data-width"))+40).height(target.attr("data-height"));
+            target.empty();
+            menu.append(list);
+            target.append(menu);
+            list.data("data-list-height",list.height());
+            list.data("can-click",true);
+            /**
+             * 绑定事件
+             * @author zhengchj
+             * @mail zhengchj@neusoft.com
+             */
+            menu.bind({
+                click:function(e){
+                    if(!list.data("can-click")){
+                        return;
+                    }
+                    menu.addClass("active");
+                    if(list.css("display")=="none"){
+                        list.data("can-click",false);
+                        list.height(0).css("display","block");
+                        list.animate({
+                            height:list.data("data-list-height")
+                        },function(){
+                            list.data("can-click",true);
+                        })
+                    }else{
+                        list.data("can-click",false);
+                        list.animate({
+                            height:0
+                        },function(){
+                            list.css("display","none");
+                            list.data("can-click",true);
+                        })
+                    }
+                    e.stopPropagation();
+                }
+            });
+            $(window).bind({
+                click:function(){
+                    if(!list.data("can-click")){
+                        return;
+                    }
+                    menu.removeClass("active");
+                    if(list.css("display")=="block"){
+                        list.data("can-click",false);
+                        list.animate({
+                            height:0
+                        },function(){
+                            list.css("display","none");
+                            list.data("can-click",true);
+                        })
+                    }
+                }
+            })
+
+        })
+    }
+
+
+})(jQuery);
