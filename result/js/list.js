0f7c92a5e426bf48934be738e9e76e9afc64b575
AfterWin <CJ_Zheng1023@hotmail.com>
2016-03-22 17:58:13 +0800

    结果浏览页面右侧菜单替换

--- result/js/list.js
+++ result/js/list.js
@@ -31,7 +31,27 @@ $(document).ready(function(){
 
     });
 
+    /**
+     * 新版右侧菜单事件绑定
+     */
+    $(".fixed-menu1").data("showing",false);
+    $(".fixed-menu1").find("a").hover(function(){
+        if($(".fixed-menu1").data("showing"))
+            return;
+        $(".fixed-menu1").data("showing",true);
+        var $strong=$(this).find("strong");
+        $strong.animate({
+            left:-($strong.width()-2)
+        },function(){
+            $(".fixed-menu1").data("showing",false);
+        });
 
+    },function(){
+        var $strong=$(this).find("strong");
+        $strong.animate({
+            left:35
+        });
+    })
 
 
 });
