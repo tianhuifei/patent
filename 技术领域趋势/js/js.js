8ef4dc1d6ac9acceae292ceb009e54b3d1900e60
CJ-Zheng1023 <CJ_Zheng1023@hotmail.com>
2016-04-03 01:13:35 +0800

    技术领域趋势完成

---
+++ 技术领域趋势/js/js.js
@@ -0,0 +1,24 @@
+/**
+ * Created by admin on 2016/2/26.
+ */
+
+$(document).ready(function(){
+    $("#items").find(".item").find("a").click(function(){
+        var me = $(this).closest(".item");
+        if(!me.hasClass("active")){
+            me.addClass("active").siblings().removeClass("active");
+        }
+    });
+
+    $("#items").find("ul").find("a").click(function(){
+        var me = $(this).parent();
+        if(!me.hasClass("active")){
+            me.addClass("active").siblings().removeClass("active");
+        }
+    });
+
+
+
+
+
+});
