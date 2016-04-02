ca6faaeacba70b76f962893173c6c94b99a127aa
CJ-Zheng1023 <CJ_Zheng1023@hotmail.com>
2016-04-03 01:42:43 +0800

    核心发明人分析完成

---
+++ 核心发明人分析/js/js.js
@@ -0,0 +1,26 @@
+/**
+ * Created by admin on 2016/2/26.
+ */
+
+var buttonGroup;
+
+$(document).ready(function(){
+    $("#items").find(".item").find("a").click(function(){
+        var me = $(this).closest(".item");
+        if(!me.hasClass("active")){
+            me.addClass("active").siblings().removeClass("active");
+        }
+    });
+
+    $("#items").find("ul").find("a").click(function(){
+        var me = $(this).parent();
+        if(!me.hasClass("active")){
+            me.addClass("active").siblings().removeClass("active");
+        }
+    });
+
+
+
+
+
+});
